a88c875f9a0221ddb829b871c0d25e0bb1871d43
Ioan Sucan <isucan@willowgarage.com>
2012-01-20 19:45:57 +0200

    demo for planning with optimization

---
+++ demos/SE2RigidBodyPlanning/KinematicCarPlanning.cpp
@@ -0,0 +1,111 @@
+/*********************************************************************
+* Rice University Software Distribution License
+*
+* Copyright (c) 2010, Rice University
+* All Rights Reserved.
+*
+* For a full description see the file named LICENSE.
+*
+*********************************************************************/
+
+/* Author: Mark Moll */
+
+#include <ompl/tools/benchmark/Benchmark.h>
+#include <ompl/control/planners/rrt/RRT.h>
+#include <ompl/control/planners/kpiece/KPIECE1.h>
+#include <omplapp/apps/KinematicCarPlanning.h>
+#include <omplapp/config.h>
+
+using namespace ompl;
+
+void kinematicCarSetup(app::KinematicCarPlanning& setup)
+{
+    // plan for kinematic car in SE(2)
+    base::StateSpacePtr SE2(setup.getStateSpace());
+
+    // set the bounds for the R^2 part of SE(2)
+    base::RealVectorBounds bounds(2);
+    bounds.setLow(-10);
+    bounds.setHigh(10);
+    SE2->as<base::SE2StateSpace>()->setBounds(bounds);
+
+    // define start state
+    base::ScopedState<base::SE2StateSpace> start(SE2);
+    start->setX(0);
+    start->setY(0);
+    start->setYaw(0);
+
+    // define goal state
+    base::ScopedState<base::SE2StateSpace> goal(SE2);
+    goal->setX(2);
+    goal->setY(2);
+    goal->setYaw(M_PI);
+
+    // set the start & goal states
+    setup.setStartAndGoalStates(start, goal, .1);
+}
+
+void kinematicCarDemo(app::KinematicCarPlanning& setup)
+{
+    setup.setPlanner(base::PlannerPtr(new control::KPIECE1(setup.getSpaceInformation())));
+    std::vector<double> cs(2);
+    cs[0] = cs[1] = 0.1;
+    setup.setup();
+    setup.getStateSpace()->getDefaultProjection()->setCellSizes(cs);
+
+    // try to solve the problem
+    if (setup.solve(20))
+    {
+        // print the (approximate) solution path: print states along the path
+        // and controls required to get from one state to the next
+        control::PathControl& path(setup.getSolutionPath());
+        //path.interpolate(); // uncomment if you want to plot the path
+        for (unsigned int i=0; i<path.states.size(); ++i)
+        {
+            const base::SE2StateSpace::StateType& s = *path.states[i]->as<base::SE2StateSpace::StateType>();
+            std::cout << s.getX() <<' '<< s.getY() << ' ' << s.getYaw() << ' ';
+            if (i==0)
+                // null controls applied for zero seconds to get to start state
+                std::cout << "0 0 0";
+            else
+            {
+                // print controls and control duration needed to get from state i-1 to state i
+                const double* c = path.controls[i-1]->as<control::RealVectorControlSpace::ControlType>()->values;
+                std::cout << c[0] << ' ' << c[1] << ' ' << path.controlDurations[i-1];
+            }
+            std::cout << std::endl;
+        }
+        if (!setup.haveExactSolutionPath())
+        {
+            std::cout << "Solution is approximate. Distance to actual goal is " <<
+                setup.getGoal()->getDifference() << std::endl;
+        }
+    }
+
+}
+void kinematicCarBenchmark(app::KinematicCarPlanning& setup)
+{
+    Benchmark::Request request(20., 10000., 10); // runtime (s), memory (MB), run count
+
+    Benchmark b(setup, setup.getName());
+    b.addPlanner(base::PlannerPtr(new control::RRT(setup.getSpaceInformation())));
+    b.addPlanner(base::PlannerPtr(new control::KPIECE1(setup.getSpaceInformation())));
+    b.benchmark(request);
+    b.saveResultsToFile();
+}
+
+int main(int argc, char* argv[])
+{
+    app::KinematicCarPlanning regularCar;
+
+    kinematicCarSetup(regularCar);
+
+    // If any command line arguments are given, solve the problem multiple
+    // times with different planners and collect benchmark statistics. 
+	// Otherwise, solve the problem once for each car type and print the path.
+    if (argc>1)
+        kinematicCarBenchmark(regularCar);
+    else
+        kinematicCarDemo(regularCar);
+    return 0;
+}
---
+++ src/omplapp/apps/KinematicCarPlanning.cpp
@@ -0,0 +1,69 @@
+/*********************************************************************
+* Rice University Software Distribution License
+*
+* Copyright (c) 2010, Rice University
+* All Rights Reserved.
+*
+* For a full description see the file named LICENSE.
+*
+*********************************************************************/
+
+/* Author: Mark Moll */
+
+#include "omplapp/apps/KinematicCarPlanning.h"
+
+ompl::app::KinematicCarPlanning::KinematicCarPlanning()
+    : AppBase<CONTROL>(constructControlSpace(), Motion_2D), timeStep_(1e-2), lengthInv_(1.), odeSolver(control::ODEBasicSolver<>(si_->getStateSpace ()))
+{
+    name_ = std::string("Kinematic car");
+    setDefaultControlBounds();
+    si_->setStatePropagator(boost::bind(&KinematicCarPlanning::propagate, this, _1, _2, _3, _4));
+
+    odeSolver.setODE(boost::bind(&ompl::app::KinematicCarPlanning::ode, this, _1, _2, _3));
+}
+
+ompl::app::KinematicCarPlanning::KinematicCarPlanning(const control::ControlSpacePtr &controlSpace)
+    : AppBase<CONTROL>(controlSpace, Motion_2D), timeStep_(1e-2), lengthInv_(1.), odeSolver(control::ODEBasicSolver<>(si_->getStateSpace ()))
+{
+    setDefaultControlBounds();
+    si_->setStatePropagator(boost::bind(&KinematicCarPlanning::propagate, this, _1, _2, _3, _4));
+}
+
+ompl::base::ScopedState<> ompl::app::KinematicCarPlanning::getDefaultStartState(void) const
+{
+    base::ScopedState<base::SE2StateSpace> sSE2(getStateSpace());
+    aiVector3D s = getRobotCenter(0);
+
+    sSE2->setX(s.x);
+    sSE2->setY(s.y);
+    sSE2->setYaw(0.);
+    return sSE2;
+}
+
+void ompl::app::KinematicCarPlanning::setDefaultControlBounds(void)
+{
+    base::RealVectorBounds cbounds(2);
+    cbounds.low[0] = -5.;
+    cbounds.high[0] = 5.;
+    cbounds.low[1] = -M_PI * 30. / 180.;
+    cbounds.high[1] = M_PI * 30. / 180.;
+    getControlSpace()->as<control::RealVectorControlSpace>()->setBounds(cbounds);
+}
+
+void ompl::app::KinematicCarPlanning::propagate(const base::State *from, const control::Control *ctrl,
+    const double duration, base::State *result)
+{
+    odeSolver.propagate (from, ctrl, duration, result);
+}
+
+void ompl::app::KinematicCarPlanning::ode(const control::ODESolver::StateType& q, const control::Control *ctrl, control::ODESolver::StateType& qdot)
+{
+    const double *u = ctrl->as<control::RealVectorControlSpace::ControlType>()->values;
+
+    // zero out qdot
+    qdot.resize (q.size (), 0);
+
+    qdot[0] = u[0] * cos(q[2]);
+    qdot[1] = u[0] * sin(q[2]);
+    qdot[2] = u[0] * lengthInv_ * tan(u[1]);
+}
