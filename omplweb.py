20ac197709ddcb9ce438536688bf28994d0f1c71
Prudhvi Boyapalli <prrb02@gmail.com>
2015-06-05 14:35:24 -0500

    Added drop down for selecting a problem from the server, and a button for uploading a custom problem

---
+++ omplweb.py
@@ -0,0 +1,329 @@
+import os
+import json
+
+import flask
+from werkzeug import secure_filename
+
+import ompl
+from ompl import base as ob
+from ompl import geometric as og
+from ompl import control as oc
+from ompl import app as oa
+
+# Location of user uploaded .dae files
+UPLOAD_FOLDER = '/Users/prudhvi/Dropbox/School/Research/KavrakiLab/OMPL/flask/omplweb/uploads'
+
+app = flask.Flask(__name__)
+app.config.from_object(__name__)
+
+
+
+########## OMPL Code ##########
+
+
+class Problem(object):
+	"""
+	Defines the motion planning problem.
+
+	Attributes:
+		The class attributes are the configuration options that define the
+		motion planing problem; they include general information and the start
+		and goal poses of the robot.
+	"""
+
+	def __init__(self):
+		"""
+		Initializes attributes to default values.
+		"""
+
+		self.name = ''
+		self.location = ''
+		self.date_modified = ''
+		self.env_path = ''
+		self.robot_path = ''
+		self.start_x = 0
+		self.start_y = 0
+		self.start_z = 0
+		self.start_theta = 0
+		self.start_axis_x = 0
+		self.start_axis_y = 0
+		self.start_axis_z = 0
+		self.goal_x = 0
+		self.goal_y = 0
+		self.goal_z = 0
+		self.goal_theta = 0
+		self.goal_axis_x = 0
+		self.goal_axis_y = 0
+		self.goal_axis_z = 0
+		self.bounds_min_x = 0
+		self.bounds_min_y = 0
+		self.bounds_min_z = 0
+		self.bounds_max_x = 0
+		self.bounds_max_y = 0
+		self.bounds_max_z = 0
+		self.time_limit = 0
+		self.mem_limit = 0
+		self.run_count = 0
+		self.planners = ''
+
+
+def allowed_file(filename):
+	"""
+	Checks that the parameter is a .dae file.
+	"""
+
+	if '.' in filename and filename.rsplit('.', 1)[1] == 'dae':
+		# Extract the file extension and return true if dae
+		return True
+
+	return False
+
+
+def parse(settings, env_path, robot_path):
+	"""
+	Reads the user submitted configuration data and creates an instance of the
+	Problem class to store the data.
+	"""
+
+	problem = Problem()
+	problem.name = settings['name']
+	problem.location = "default_location"
+	problem.date_modified = "2/15/2015"
+
+	# start filename stuff #
+	problem.env_path = env_path
+	problem.robot_path = robot_path
+
+	# start problem specific stuff #
+	problem.start_x = float(settings['start_x'])
+	problem.start_y = float(settings['start_y'])
+	problem.start_z = float(settings['start_z'])
+	problem.start_theta = float(settings['start_theta'])
+	problem.start_axis_x = float(settings['start_axis_x'])
+	problem.start_axis_y = float(settings['start_axis_y'])
+	problem.start_axis_z = float(settings['start_axis_z'])
+	problem.goal_x = float(settings['goal_x'])
+	problem.goal_y = float(settings['goal_y'])
+	problem.goal_z = float(settings['goal_z'])
+	problem.goal_theta = float(settings['goal_theta'])
+	problem.goal_axis_x = float(settings['goal_axis_x'])
+	problem.goal_axis_y = float(settings['goal_axis_y'])
+	problem.goal_axis_z = float(settings['goal_axis_z'])
+	problem.bounds_min_x = float(settings['bounds_min_x'])
+	problem.bounds_min_y = float(settings['bounds_min_y'])
+	problem.bounds_min_z = float(settings['bounds_min_z'])
+	problem.bounds_max_x = float(settings['bounds_max_x'])
+	problem.bounds_max_y = float(settings['bounds_max_y'])
+	problem.bounds_max_z = float(settings['bounds_max_z'])
+
+	# benchmark specific stuff #
+	problem.time_limit = float(settings['time_limit'])
+	problem.mem_limit = float(settings['mem_limit'])
+	problem.run_count = float(settings['run_count'])
+
+	# planners for benchmarks #
+	problem.planners = settings['planners']
+
+	return problem
+
+
+def format_solution(path, solved):
+	"""
+	Formats the either the solution, or a failure message for delivery to the
+	client.
+	"""
+
+	solution = {}
+
+	if solved:
+		solution['solved'] = 'true'
+		solution['length'] = path.length()
+	else:
+		solution['solved'] = 'false'
+		solution['length'] = 0
+
+	# TODO: Better, neater formatting of path
+	solution['path'] = str(path)
+
+	return solution
+
+
+def solve(problem):
+	"""
+	Given an instance of the Problem class, containing the problem configuration
+	data, solves the motion planning problem and returns either the solution
+	path or a failure message.
+	"""
+
+	# Buffer to hold information and user messages
+	messages = "Messages: \n"
+
+	## Configure the problem
+	space = ob.SE3StateSpace()
+
+	# Set the dimensions of the bounding box
+	bounds = ob.RealVectorBounds(3)
+
+	bounds.low[0] = problem.bounds_min_x
+	bounds.low[1] = problem.bounds_min_y
+	bounds.low[2] = problem.bounds_min_z
+
+	bounds.high[0] = problem.bounds_max_x
+	bounds.high[1] = problem.bounds_max_y
+	bounds.high[2] = problem.bounds_max_z
+
+	space.setBounds(bounds)
+
+	# Create an instance of SimpleSetup
+	# ompl_setup = og.SimpleSetup(space)
+	ompl_setup = oa.SE3RigidBodyPlanning()
+
+	# TODO: Set state validity checker here?
+	ompl_setup.setEnvironmentMesh(str(problem.env_path))
+	ompl_setup.setRobotMesh(str(problem.robot_path))
+
+	# Set the start and goal states
+	start = ob.State(space)
+	start().setXYZ(problem.start_x, problem.start_y, problem.start_z)
+	start().rotation().setAxisAngle(
+			problem.start_axis_x, problem.start_axis_y,
+			problem.start_axis_z, problem.start_theta
+	)
+
+	goal = ob.State(space)
+	goal().setXYZ(problem.goal_x, problem.goal_y, problem.goal_z)
+	goal().rotation().setAxisAngle(
+			problem.goal_axis_x, problem.goal_axis_y,
+			problem.goal_axis_z, problem.goal_theta
+	)
+
+	ompl_setup.setStartAndGoalStates(start, goal)
+
+
+	# Load the planner
+	space_info = ompl_setup.getSpaceInformation()
+
+	if problem.planners != "":
+		# If user selected a planner, load it
+		planner = eval("ompl.%s(space_info)" % problem.planners)
+		ompl_setup.setPlanner(planner)
+		print "Planner range: %f" % planner.getRange()
+		messages += "Using planner: %s" % ompl_setup.getPlanner().getName()
+	else:
+		messages += "No planner specified, using default"
+
+	print ompl_setup.getStateValidityChecker()
+
+	## Solve the problem
+	solution = {}
+	solved = ompl_setup.solve(problem.time_limit)
+
+
+	## Check for validity
+	if solved:
+		path = ompl_setup.getSolutionPath();
+		# print ompl_setup.getSolutionPath().printAsMatrix()
+		initialValid = path.check()
+
+		if initialValid:
+			solution = format_solution(path, True)
+
+			# If if initially valid, attempt to simplify
+			# ompl_setup.simplifySolution()
+			# Get the simplified path
+			# simple_path = ompl_setup.getSolutionPath()
+			# simplifyValid = simple_path.check()
+			# if simplifyValid:
+				# messages += "Simplified path was found.\n"
+				# solution = format_solution(simple_path, True)
+			# else:
+				# messages += "Simplified path was invalid. Returned non-simplified path.\n"
+				# solution = format_solution(path, True)
+
+			# TODO: Interpolation?
+
+		else :
+			messages += "Path reported by planner seems to be invalid.\n"
+			solution = format_solution(path, False)
+	else:
+		messages += "No valid path was found with the provided configuration.\n"
+		solution = format_solution(None, False)
+
+	print messages
+
+	solution['name'] = problem.name
+	solution['planner'] = ompl_setup.getPlanner().getName()
+	solution['messages'] = messages
+
+	return json.dumps(solution)
+
+
+########## Flask Code ##########
+
+
+@app.route("/omplapp", methods=['GET'])
+def omplapp():
+	"""
+	Returns the problem configuration page.
+	"""
+
+	return flask.render_template("omplweb.html")
+
+
+@app.route('/omplapp/upload', methods=['POST'])
+def upload():
+	"""
+	This function is invoked when the client clicks 'Solve' and submits the
+	problem configuration data.
+
+	1.	The robot and environment files are checked for validity and downloaded to
+		the server.
+	2.	Configuration data is parsed and loaded via the parse function.
+	3.	The problem is solved and the output of the solve function (either a
+		solution path or an error) is returned.
+
+	"""
+
+	robotFile = flask.request.files['robot']
+	envFile = flask.request.files['env']
+
+	if robotFile and envFile:
+		if allowed_file(robotFile.filename) and allowed_file(envFile.filename):
+
+			# If valid files, save them to the server
+			robot_filename = secure_filename(robotFile.filename)
+			robotFile.save(os.path.join(app.config['UPLOAD_FOLDER'], robot_filename))
+			robot_path = os.path.join(app.config['UPLOAD_FOLDER'], robot_filename)
+
+			env_filename = secure_filename(envFile.filename)
+			envFile.save(os.path.join(app.config['UPLOAD_FOLDER'], env_filename))
+			env_path = os.path.join(app.config['UPLOAD_FOLDER'], env_filename)
+
+			print "SERVER: Files saved as: " + robot_path + " and " + env_path
+
+			print "SERVER: Will now parse configuration..."
+			#TODO# Put some try/catches here
+			problem = parse(flask.request.form, env_path, robot_path)
+
+			print "SERVER: Configuration has been parsed. Solving problem..."
+			solution = solve(problem);
+			# TODO # Delete the uploaded files?
+			return solution;
+
+		else:
+			return "Error: Wrong file format. Robot and environment files must be .dae"
+	else:
+		return "Error: Didn't upload any files! Please choose both a robot and an environment file in the .dae format."
+
+	return "Upload Successful."
+
+
+@app.route("/")
+def index():
+	return flask.redirect(flask.url_for('omplapp'))
+
+
+if __name__ == "__main__":
+	app.debug = True
+	app.run()
+
---
+++ static/scripts/omplweb.js
@@ -0,0 +1,365 @@
+
+var planners= {
+	"BKPIECE1" : {
+		"Border fraction" : 0.90,
+		"Range" : 0.0
+	},
+	"EST" : {
+		"Goal bias" : 0.05,
+		"Range" : 0.0
+	},
+	"FMT" : {
+		"Free space volume" : 1.0,
+		"Num samples" : 1000.0,
+		"Radius multiplier" : 1.10
+	},
+	"KPIECE1" : {
+		"Border fraction" : 0.90,
+		"Goal bias" : 0.05,
+		"Range" : 0.0
+	},
+	"LBKPIECE1" : {
+		"Border fraction" : 0.90,
+		"Range" : 0.0
+	},
+	"LBTRRT" : {
+		"Epsilon" : 0.40,
+		"Goal bias" : 0.05,
+		"Range" : 0.0
+	},
+	"LazyPRM" : {
+		"Max nearest neighbors" : 8,
+		"Range" : 0.0
+	},
+	"LazyPRMstar" : {
+		"Max nearest neighbors" : 8,
+		"Range" : 0
+	},
+	"LazyRRT" : {
+		"Goal bias" : 0.05,
+		"Range" : 0.0
+	},
+	"PDST" : {
+		"Goal bias" : 0.05,
+	},
+	"PRM" : {
+		"Max nearest neighbors" : 8,
+	},
+	"PRMstar" : {
+		"Max nearest neighbors" : 8,
+	},
+	"RRT" : {
+		"Goal bias" : 0.05,
+		"Range" : 0.0
+	},
+	"RRTConnect" : {
+		"Range" : 0.0
+	},
+	"RRTstar" : {
+		"Delay collision checking" : "False",
+		"Goal bias" : 0.05,
+		"Prune" : "False",
+		"Range" : 0.0
+	},
+	"SBL" : {
+		"Range" : 0.0
+	},
+	"SPARS" : {
+		"Dense delta fraction" : 0.0010,
+		"Max failures" : 1000.0,
+		"Sparse delta fraction" : 0.25,
+		"Stretch factor" : 3.0
+	},
+	"SPARStwo" : {
+		"Dense delta fraction" : 0.0010,
+		"Max failures" : 3000.0,
+		"Sparse delta fraction" : 0.25,
+		"Stretch factor" : 3.0
+
+	},
+	"STRIDE" : {
+		"Degree" : 16.0,
+		"Estimated dimension" : 1.0,
+		"Goal bias" : 0.05,
+		"Max degree" : 18.0,
+		"Max pts per leaf" : 6.0,
+		"Min degree" : 12.0,
+		"Min valid path fraction" : 0.20,
+		"Range" : 0.0,
+		"Use projected distance" : "False"
+	},
+	"TRRT" : {
+		"Goal bias" : 0.05,
+		"Max states failed" : 10.0,
+		"Range" : 0.0,
+		"Temp change factor" : 2.00
+	}
+}
+
+
+
+$(document).ready(function() {
+	// Load config data when .cfg file is selected
+	$("#config").change(function (){
+		loadConfig();
+	});
+
+
+	// Default planner
+	load_planner_params("KPIECE1", planners["KPIECE1"]);
+	$("#generalConfig").collapse("show");
+
+	// When user picks planner, update our info
+	$("#planners").change(function() {
+		planner_name = $("#planners").val().split(".")[1];
+		planner_params = planners[planner_name];
+		load_planner_params(planner_name, planner_params);
+	});
+})
+
+
+function load_planner_params(planner_name, planner_params) {
+	var plannerConfigHTML = "";
+	plannerConfigHTML += "<div class='col-md-4'>";
+	plannerConfigHTML += "<h4>" + planner_name + " Parameters</h4>";
+	plannerConfigHTML += "<table class='table'>";
+
+	for (var key in planner_params) {
+		if (planner_params.hasOwnProperty(key)) {
+			plannerConfigHTML += "<tr><td>" + key  + "</td>";
+			plannerConfigHTML += "<td><input type='text' name='" + key +  "' class='form-field' value='" + planner_params[key] + "'></td></tr>";
+		}
+	}
+
+	plannerConfigHTML += "</table>";
+	plannerConfigHTML += "</div>";
+
+	$("#plannerConfig").html(plannerConfigHTML);
+	$("#generalConfig").collapse("hide");
+	$("#plannerConfig").collapse("show");
+}
+
+
+function solve(){
+	// Check that all fields are filled in
+	var validConfig = validateFields();
+
+	if (validConfig == true) {
+		var html = "";
+		html += "<h2><center>Results</h2>"
+		$('#results').html(html);
+
+		// Bring up the loading screen
+		$.blockUI({
+			css: {
+				border: 'none',
+				padding: '30px',
+				backgroundColor: '#000',
+				opacity: '0.7',
+				color: '#fff',
+			}
+		});
+
+		// Read the input fields
+		var formData = new FormData($('form')[0]);
+
+		// Send the request
+		$.ajax({
+			url: "omplapp/upload",
+			type: "POST",
+			data: formData,
+			success: function(data){
+				data = JSON.parse(data);
+
+				html += "<pre>";
+				html += data.name;
+
+				if (data.solved == "true") {
+					html += "<font color='green'>: Found solution.</font><br><br>";
+				} else {
+					html += "<font color='red'>: No solution found.</font><br><br>";
+				}
+
+				html += data.messages;
+				html += "<br><br>"
+
+				html += "Path Length: " + data.length + "<br>";
+
+				html += data.path;
+
+				html += "</pre>";
+
+				$('#generalConfig').collapse('hide');
+				$('#plannerConfig').collapse('hide');
+				$.unblockUI();
+				$('#results').html(html);
+			},
+			error: function(data) {
+				$.unblockUI();
+				alert("Server responded with an error.");
+				console.log(data);
+			},
+			cache: false,
+			contentType: false,
+			processData: false
+		});
+	} else {
+		// Invalid fields have been highlighted by 'validateField()'.
+	}
+}
+
+function loadConfig() {
+	var cfgFile = $("#config")[0].files[0];
+
+	if (cfgFile != null) {
+		var reader = new FileReader();
+
+		reader.readAsText(cfgFile);
+
+		reader.onload = function () {
+			var cfgData = parseConfig(reader.result);
+
+			//TODO: Make this neater
+			document.getElementsByName("name")[0].value = cfgData['name'];
+			// document.getElementsByName("planners")[0].value = "";
+			document.getElementsByName("start_x")[0].value = cfgData['start.x'];
+			document.getElementsByName("start_y")[0].value = cfgData['start.y'];
+			document.getElementsByName("start_z")[0].value = cfgData['start.z'];
+			document.getElementsByName("start_theta")[0].value = cfgData['start.theta'];
+			document.getElementsByName("start_axis_x")[0].value = cfgData['start.axis.x'];
+			document.getElementsByName("start_axis_y")[0].value = cfgData['start.axis.y'];
+			document.getElementsByName("start_axis_z")[0].value = cfgData['start.axis.z'];
+			document.getElementsByName("goal_x")[0].value = cfgData['goal.x'];
+			document.getElementsByName("goal_y")[0].value = cfgData['goal.y'];
+			document.getElementsByName("goal_z")[0].value = cfgData['goal.z'];
+			document.getElementsByName("goal_theta")[0].value = cfgData['goal.theta'];
+			document.getElementsByName("goal_axis_x")[0].value = cfgData['goal.axis.x'];
+			document.getElementsByName("goal_axis_y")[0].value = cfgData['goal.axis.y'];
+			document.getElementsByName("goal_axis_z")[0].value = cfgData['goal.axis.z'];
+			document.getElementsByName("bounds_min_x")[0].value = cfgData['volume.min.x'];
+			document.getElementsByName("bounds_min_y")[0].value = cfgData['volume.min.y'];
+			document.getElementsByName("bounds_min_z")[0].value = cfgData['volume.min.z'];
+			document.getElementsByName("bounds_max_x")[0].value = cfgData['volume.max.x'];
+			document.getElementsByName("bounds_max_y")[0].value = cfgData['volume.max.y'];
+			document.getElementsByName("bounds_max_z")[0].value = cfgData['volume.max.z'];
+			document.getElementsByName("time_limit")[0].value = cfgData['time_limit'];
+			document.getElementsByName("mem_limit")[0].value = cfgData['mem_limit'];
+			document.getElementsByName("run_count")[0].value = cfgData['run_count'];
+
+		}
+	} else {
+		alert("Please select a valid configuration file.")
+	}
+}
+
+function parseConfig(cfgText) {
+	var cfgData = {};
+
+	// Separate into lines
+	var cfgLines = cfgText.split("\n");
+
+	for (var i=0; i < cfgLines.length; i++) {
+		if(cfgLines[i][0] != "[") {
+			// Remove all extra spacing
+			var line = cfgLines[i].replace(/\s+/g, '');
+
+			// Split into (key, value) pairs
+			var items = line.split("=");
+			cfgData[items[0]]= items[1];
+		} else {
+			// This config line isn't used
+			// console.log("Ignored: ", cfgLines[i]);
+		}
+	}
+
+	return cfgData;
+}
+
+function clearAllFields() {
+	$('.form-field').each(function () {
+		$(this).val('');
+		$(this).css("background-color", "white");
+	});
+	$('#config').val('');
+	$('#results').html('');
+	$('#generalConfig').collapse('show');
+}
+
+function validateFields() {
+	var valid = true;
+
+	// Check that all the fields have values
+	$('.form-field').each(function () {
+		if ($(this).val() === '' || $(this).val() == null) {
+			valid = false;
+
+			// Highlight the incorrect field
+			$(this).css("background-color", "#fda9a0");
+		} else {
+			$(this).css("background-color", "white");
+		}
+	});
+
+
+	return valid
+}
+
+
+
+
+
+	// inputData = {}
+	// inputData.name = $("#name").val();
+	//		   inputData.start_x = $("#start_x").val();
+	//		   inputData.start_y = $("#start_y").val();
+	//		   inputData.start_z = $("#start_z").val();
+	//		   inputData.start_theta = $("#start_theta").val();
+	//		   inputData.start_axis_x = $("#start_axis_x").val();
+	//		   inputData.start_axis_y = $("#start_axis_y").val();
+	//		   inputData.start_axis_z = $("#start_axis_z").val();
+	//		   inputData.goal_x = $("#goal_x").val();
+	//		   inputData.goal_y = $("#goal_y").val();
+	//		   inputData.goal_z = $("#goal_z").val();
+	//		   inputData.goal_theta = $("#goal_theta").val();
+	//		   inputData.goal_axis_x = $("#goal_axis_x").val();
+	//		   inputData.goal_axis_y = $("#goal_axis_y").val();
+	//		   inputData.goal_axis_z = $("#goal_axis_z").val();
+	//		   inputData.bounds_min_x = $("#bounds_min_x").val();
+	//		   inputData.bounds_min_y = $("#bounds_min_y").val();
+	//		   inputData.bounds_min_z = $("#bounds_min_z").val();
+	//		   inputData.bounds_max_x = $("#bounds_max_x").val();
+	//		   inputData.bounds_max_y = $("#bounds_max_y").val();
+	//		   inputData.bounds_max_z = $("#bounds_max_z").val();
+	//		   inputData.time_limit = $("#time_limit").val();
+	//		   inputData.mem_limit = $("#mem_limit").val();
+	//		   inputData.run_count = $("#run_count").val();
+	//		   inputData.planners = $("#planners").val();
+	//
+	//		   inputData.location = "default_location";
+	//		   inputData.date_modified = "2/15/2015";
+	//		   inputData.env_path = "../../../robots/3D/Apartment_env.dae";
+	//		   inputData.robot_path = "../../../robots/3D/Apartment_robot.dae";
+	//
+	// console.log(inputData);
+
+	// var promise = postData(JSON.stringify(inputData));
+	//
+//	promise.success(function (data){
+//		console.log(data);
+//		$('#results').html(
+//			'<pre>' +
+//			JSON.stringify(data, ['solved', 'path'], ' ') +
+//			'</pre>'
+//		);
+//	})
+// }
+
+// function postData(sendData) {
+//	return $.ajax({
+//		url: "omplapp/solve",
+//		type: "POST",
+//		dataType: "json",
+//		data: {settings : sendData}
+//	});
+
+
